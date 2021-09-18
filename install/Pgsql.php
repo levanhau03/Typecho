c0b7eab8736313e34247cf65c773b23cc5b24df2
levanhau03 <nhatki888@gmail.com>
2021-09-18 11:59:23 +0700

    Update Pgsql.php

--- install/Pgsql.php
+++ install/Pgsql.php
@@ -2,25 +2,25 @@
 <li>
 <label class="typecho-label" for="dbHost"><?php _e('Địa chỉ cơ sở dữ liệu'); ?></label>
 <input type="text" class="text" name="dbHost" id="dbHost" value="<?php _v('dbHost', 'localhost'); ?>"/>
-<p class="description"><?php _e('您可能会使用 "%s"', 'localhost'); ?></p>
+<p class="description"><?php _e('Bạn có thể sử dụng "%s"', 'localhost'); ?></p>
 </li>
 <li>
-<label class="typecho-label" for="dbPort"><?php _e('数据库端口'); ?></label>
+<label class="typecho-label" for="dbPort"><?php _e('Cổng cơ sở dữ liệu'); ?></label>
 <input type="text" class="text" name="dbPort" id="dbPort" value="<?php _v('dbPort', '5432'); ?>"/>
-<p class="description"><?php _e('如果您不知道此选项的意义, 请保留默认设置'); ?></p>
+<p class="description"><?php _e('Nếu bạn không biết ý nghĩa của tùy chọn này, vui lòng giữ cài đặt mặc định'); ?></p>
 </li>
 <li>
-<label class="typecho-label" for="dbUser"><?php _e('数据库用户名'); ?></label>
+<label class="typecho-label" for="dbUser"><?php _e('Người dùng cơ sở dữ liệu'); ?></label>
 <input type="text" class="text" name="dbUser" id="dbUser" value="<?php _v('dbUser', 'postgres'); ?>" />
-<p class="description"><?php _e('您可能会使用 "%s"', 'postgres'); ?></p>
+<p class="description"><?php _e('Bạn có thể sử dụng "%s"', 'postgres'); ?></p>
 </li>
 <li>
-<label class="typecho-label" for="dbPassword"><?php _e('数据库密码'); ?></label>
+<label class="typecho-label" for="dbPassword"><?php _e('Mật khẩu cơ sở dữ liệu'); ?></label>
 <input type="password" class="text" name="dbPassword" id="dbPassword" value="<?php _v('dbPassword'); ?>" />
 </li>
 <li>
-<label class="typecho-label" for="dbDatabase"><?php _e('数据库名'); ?></label>
+<label class="typecho-label" for="dbDatabase"><?php _e('Tên cơ sở dữ liệu'); ?></label>
 <input type="text" class="text" name="dbDatabase" id="dbDatabase" value="<?php _v('dbDatabase', 'typecho'); ?>" />
-<p class="description"><?php _e('请您指定数据库名称'); ?></p>
+<p class="description"><?php _e('Vui lòng chỉ định tên cơ sở dữ liệu'); ?></p>
 </li>
 <input type="hidden" name="dbCharset" value="<?php _e('utf8'); ?>" />
