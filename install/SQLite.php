8cd72dff7eedc65e569f835d36881b7a1a143458
levanhau03 <nhatki888@gmail.com>
2021-09-18 11:59:56 +0700

    Update SQLite.php

--- install/SQLite.php
+++ install/SQLite.php
@@ -1,7 +1,7 @@
 <?php if(!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $defaultDir = dirname($_SERVER['SCRIPT_FILENAME']) . '/usr/' . uniqid() . '.db'; ?>
 <li>
-<label class="typecho-label" for="dbFile"><?php _e('数据库文件路径'); ?></label>
+<label class="typecho-label" for="dbFile"><?php _e('Đường dẫn tệp cơ sở dữ liệu'); ?></label>
 <input type="text" class="text" name="dbFile" id="dbFile" value="<?php _v('dbFile', $defaultDir); ?>"/>
-<p class="description"><?php _e('"%s" 是我们为您自动生成的地址', $defaultDir); ?></p>
+<p class="description"><?php _e('"%s" là địa chỉ chúng tôi tự động tạo cho bạn', $defaultDir); ?></p>
 </li>
