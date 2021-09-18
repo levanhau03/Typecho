8cadce3ae18a2c8fe4066bccc7b22905d79d67e5
levanhau03 <nhatki888@gmail.com>
2021-09-18 14:06:39 +0700

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -11,29 +11,29 @@ $stat = Typecho_Widget::widget('Widget_Stat');
         <?php include 'page-title.php'; ?>
         <div class="row typecho-page-main">
             <div class="col-mb-12 col-tb-3">
-                <p><a href="http://gravatar.com/emails/" title="<?php _e('在 Gravatar 上修改头像'); ?>"><?php echo '<img class="profile-avatar" src="' . Typecho_Common::gravatarUrl($user->mail, 220, 'X', 'mm', $request->isSecure()) . '" alt="' . $user->screenName . '" />'; ?></a></p>
+                <p><a href="http://gravatar.com/emails/" title="<?php _e('Sửa đổi hình đại diện trên Gravatar'); ?>"><?php echo '<img class="profile-avatar" src="' . Typecho_Common::gravatarUrl($user->mail, 220, 'X', 'mm', $request->isSecure()) . '" alt="' . $user->screenName . '" />'; ?></a></p>
                 <h2><?php $user->screenName(); ?></h2>
                 <p><?php $user->name(); ?></p>
-                <p><?php _e('目前有 <em>%s</em> 篇日志, 并有 <em>%s</em> 条关于你的评论在 <em>%s</em> 个分类中.', 
+                <p><?php _e('Hiện tại có <em>%s</em> bài đăng, <em>%s</em> nhận xét về bạn nằm trong <em>%s</em> chuyên mục.', 
                 $stat->myPublishedPostsNum, $stat->myPublishedCommentsNum, $stat->categoriesNum); ?></p>
                 <p><?php
                 if ($user->logged > 0) {
                     $logged = new Typecho_Date($user->logged);
-                    _e('最后登录: %s', $logged->word());
+                    _e('Lần đăng nhập cuối: %s', $logged->word());
                 }
                 ?></p>
             </div>
 
             <div class="col-mb-12 col-tb-6 col-tb-offset-1 typecho-content-panel" role="form">
                 <section>
-                    <h3><?php _e('个人资料'); ?></h3>
+                    <h3><?php _e('Thông tin cá nhân'); ?></h3>
                     <?php Typecho_Widget::widget('Widget_Users_Profile')->profileForm()->render(); ?>
                 </section>
 
                 <?php if($user->pass('contributor', true)): ?>
                 <br>
                 <section id="writing-option">
-                    <h3><?php _e('撰写设置'); ?></h3>
+                    <h3><?php _e('Cài đặt soạn thảo'); ?></h3>
                     <?php Typecho_Widget::widget('Widget_Users_Profile')->optionsForm()->render(); ?>
                 </section>
                 <?php endif; ?>
@@ -41,7 +41,7 @@ $stat = Typecho_Widget::widget('Widget_Stat');
                 <br>
 
                 <section id="change-password">
-                    <h3><?php _e('密码修改'); ?></h3>
+                    <h3><?php _e('Đổi mật khẩu'); ?></h3>
                     <?php Typecho_Widget::widget('Widget_Users_Profile')->passwordForm()->render(); ?>
                 </section>
 
